e72780255ce05c4c5b38c88905e9345409509f25
admin <kondosatoru@test.local>
2016-07-07 18:26:10 +0900

    make env

---
+++ ws0/install02.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# Creating environment (sandbox instance called py3 [choose the name you want])
+conda create -n py2 python=2 ipython
+
+# Activating created environment
+source activate py2
+
+# Install package manager pip
+conda install pip
+
+# which ipython is to be used in the environment? pip freeze shows it
+pip freeze
+
+# Installing the packages
+conda install bs4
+conda install Scrapy
+conda install feedparser
